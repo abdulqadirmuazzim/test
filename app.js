3475ed501495881586fbc000a0fda6015981dcd1
abdulqadir <abdulqadirbinmuslim@gmail.com>
2023-08-17 20:42:05 +0100

    Third commit

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const exp = require('express')
-app = exp()
+const { v4 } = require('uuid');
+
+const app = exp()
+
+app.get('/api', (req, res) => {
+    const path = `/api/item/${v4()}`;
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+    res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+app.get('/api/item/:slug', (req, res) => {
+    const { slug } = req.params;
+    res.end(`Item: ${slug}`);
+});
+
+
 
 app.get('/', (req, res) => {
     res.send("Salamualikum from express")
@@ -12,3 +28,5 @@ app.get('/about', (re, rs) => {
 app.listen(2000, () => console.log("running on http://2000"))
 
 
+
+module.exports = exp;
