b0f53cbd204a76a3b96d320854d3bd76a288397c
abdulqadir <abdulqadirbinmuslim@gmail.com>
2023-08-18 20:58:12 +0100

    configured vercel.json 03

--- api/app.js
+++ api/app.js
@@ -6,8 +6,8 @@ const app = exp()
 app.get('/api', (req, res) => {
     const path = `/api/item/${v4()}`;
     res.setHeader('Content-Type', 'text/html');
-    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-    res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+    res.setHeader('Cache-Control', 's-max-age=1,Hello! Go to item: <a href="${path stale-while-revalidate');
+    res.end(`}">${path}</a>`);
 });
 
 app.get('/api/item/:slug', (req, res) => {
@@ -18,20 +18,20 @@ app.get('/api/item/:slug', (req, res) => {
 
 module.exports = app;
 
-// app.get('/', (req, res) => {
-//     res.send(`<h1>Salamualikum from express</h1>
-//     <style>
-//     body{
-//         margin:0;
-//         background: linear-gradient(to right, orange, red);
-//     }
-//     </style>`)
-// })
-// app.get('/about', (re, rs) => {
-//     rs.send("<h2>This in about testing our app on vercel</h2>")
+app.get('/', (req, res) => {
+    res.send(`<h1>Salamualikum from express</h1>
+    <style>
+    body{
+        margin:0;
+        background: linear-gradient(to right, orange, red);
+    }
+    </style>`)
+})
+app.get('/about', (re, rs) => {
+    rs.send("<h2>This in about testing our app on vercel</h2>")
 
-// })
-// let port = process.env.PORT || 2000
-// app.listen(2000, () => console.log("running on http://2000"))
+})
+let port = process.env.PORT || 2000
+app.listen(2000, () => console.log("running on http://2000"))
 
 
